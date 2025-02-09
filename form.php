db36fce289d5046bdb7cf0bb17097d42b6cd44d5
23jn0114 <23jn0114@jec.ac.jp>
2025-02-09 08:01:13 +0000

    テーブル形式で表示

--- form.php
+++ form.php
@@ -2,38 +2,64 @@
 if ($_SERVER['REQUEST_METHOD']==='POST') {
     // 受信したJSON文字列
     $post_data = json_decode($_POST['json'], true);
-    foreach($post_data as $i => $json_data) {
-        // JSON文字列をPHPの配列に変換
-        $data = $json_data;
-        
-        // 各キーに対応する変数にデータを格納
-        $MerchantName = $data['MerchantName'];
-        $MerchantAddress = $data['MerchantAddress'];
-        $MerchantAddress_Obj = $data['MerchantAddress_Obj'];
-        $MerchantPhoneNumber = $data['MerchantPhoneNumber'];
-        $TransactionDate = $data['TransactionDate'];
-        $TransactionTime = $data['TransactionTime'];
-        $Items = $data['Items'];
-        
-        // $item_name = $Items[$i]['name'];
-        // $item_price = $Items[$i]['price'];
-        
-        // 確認のために各変数を出力
-        echo "MerchantName: " . $MerchantName . "\n";
-        echo "MerchantAddress: " . $MerchantAddress . "\n";
-        echo "MerchantPhoneNumber: " . $MerchantPhoneNumber . "\n";
-        echo "TransactionDate: " . $TransactionDate . "\n";
-        echo "TransactionTime: " . $TransactionTime . "\n";
-        echo "Items: " . print_r($Items, true) . "\n";
-        echo "houseNumber: " . $houseNumber . "\n";
-        echo "city: " . $city . "\n";
-        echo "state: " . $state . "\n";
-        echo "streetAddress: " . $streetAddress . "\n";
-        echo "cityDistrict: " . $cityDistrict . "\n";
-        echo "item_name: " . $item_name . "\n";
-        echo "item_price: " . $item_price . "\n";
-    }
+    
 } else {
     echo "表示できました";
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>レシートの出力結果</title>
+</head>
+<body>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') { ?>
+        <table border="1">
+            <tr>
+                <th>Merchant Name</th>
+                <th>Merchant Address</th>
+                <th>Merchant Phone Number</th>
+                <th>Transaction Date</th>
+                <th>Transaction Time</th>
+                <th>Item Name</th>
+                <th>Item Price</th>
+            </tr>
+            <?php
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                $post_data = json_decode($_POST['json'], true);
+                foreach ($post_data as $i => $json_data) {
+                    $data = $json_data;
+                    $MerchantName = $data['MerchantName'];
+                    $MerchantAddress = $data['MerchantAddress'];
+                    $MerchantPhoneNumber = $data['MerchantPhoneNumber'];
+                    $TransactionDate = $data['TransactionDate'];
+                    $TransactionTime = $data['TransactionTime'];
+                    $Items = $data['Items'];
+                    $rowspan = count($Items);
+
+                    foreach ($Items as $index => $item) {
+                        echo "<tr>";
+                        if ($index == 0) {
+                            echo "<td rowspan='$rowspan'>" . $MerchantName . "</td>";
+                            echo "<td rowspan='$rowspan'>" . $MerchantAddress . "</td>";
+                            echo "<td rowspan='$rowspan'>" . $MerchantPhoneNumber . "</td>";
+                            echo "<td rowspan='$rowspan'>" . $TransactionDate . "</td>";
+                            echo "<td rowspan='$rowspan'>" . $TransactionTime . "</td>";
+                        }
+                        echo "<td>" . $item['name'] . "</td>";
+                        echo "<td>" . $item['price'] . "</td>";
+                        echo "</tr>";
+                    }
+                }
+            }
+            ?>
+        </table>
+    <?php } ?>
+    
+    
+</body>
+</html>
